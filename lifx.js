3441325f6ae38934b42b365d61ff26b3249e0d40
Niklas <me@sodapop.se>
2018-09-16 14:00:29 +0200

    Wow

--- lifx.js
+++ lifx.js
@@ -1,25 +1,64 @@
 #!/usr/bin/env node
 
-require("yargs")
+const yargs = require("yargs");
+const lifx = require("node-lifx-lan");
+
+function noop() {}
+
+function exit() {
+  process.exit(1);
+}
+
+yargs
+  .version()
+  .help()
+  .option("ip", {
+    describe: "Bulb IP-address"
+  })
+  .option("mac", {
+    describe: "Bulb MAC-address"
+  })
+  .demandOption(["ip", "mac"])
   .command(
     "color <hue> <saturation> <brightness> <kelvin>",
     "Set lights color",
-    () => {},
-    ({ hue, saturation, brightness, kelvin }) =>
-      require("node-lifx-lan").setColorBroadcast({
-        color: {
-          hue: Math.min(1, Math.max(parseFloat(hue), 0)), // 0.0-1.1
-          saturation: Math.min(1, Math.max(parseFloat(saturation), 0)), // 0.0-1.1
-          brightness: Math.min(1, Math.max(parseFloat(brightness), 0)), // 0.0-1.1
-          kelvin: Math.min(9000, Math.max(parseInt(kelvin), 2500)) // 2500-9000
-        }
-      })
+    noop,
+    ({ hue, saturation, brightness, kelvin, ip, mac }) =>
+      lifx
+        .createDevice({
+          ip,
+          mac
+        })
+        .then(device =>
+          device.setColor({
+            color: {
+              hue: Math.min(1, Math.max(parseFloat(hue), 0)), // 0.0-1.0
+              saturation: Math.min(1, Math.max(parseFloat(saturation), 0)), // 0.0-1.0
+              brightness: Math.min(1, Math.max(parseFloat(brightness), 0)), // 0.0-1.0
+              kelvin: Math.min(9000, Math.max(parseInt(kelvin), 2500)) // 2500-9000
+            }
+          })
+        )
+        .then(exit)
   )
-  // .command("power <on|off>", "Power lights on or off", ({ onoff }) => {
-  //   if (onoff === "on") {
-  //     return Lifx.turnOnBroadcast();
-  //   } else {
-  //     return Lifx.turnOffBroadcast();
-  //   }
-  // })
-  .help().argv;
+  .command("power <on|off>", "Power lights on or off", yargs =>
+    yargs
+      .command("on", "Power lights on", noop, () =>
+        lifx
+          .createDevice({
+            ip,
+            mac
+          })
+          .then(device => device.turnOn())
+          .then(exit)
+      )
+      .command("off", "Power lights off", noop, () =>
+        lifx
+          .createDevice({
+            ip,
+            mac
+          })
+          .then(device => device.turnOff())
+          .then(exit)
+      )
+  ).argv;
